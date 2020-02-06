0d6598178e6c5f79e162005da1174b7d4855a078
Joseph Mathew <josephmathew900@gmail.com>
2020-02-06 16:10:11 +0530

    added firebase auth

--- app.js
+++ app.js
@@ -2,16 +2,28 @@ var express = require('express');
 var app = express();
 let authorized = true;
 const cors = require('cors');
+const admin = require('firebase-admin');
+
+const serviceAccount = require("./config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://nodefirebase-d00d5.firebaseio.com"
+});
 
 app.use(cors());
 
-function checkAuth(req, res, next){
-    if(authorized){
-        next();
-    }else{
-        res.status(403).send('Unauthorized');
-        return;
-    }
+function checkAuth(req, res, next) {
+  if (req.headers.authtoken) {
+    admin.auth().verifyIdToken(req.headers.authtoken)
+      .then(() => {
+        next()
+      }).catch(() => {
+        res.status(403).send('Unauthorized')
+      });
+  } else {
+    res.status(403).send('Unauthorized')
+  }
 }
 
 app.use('/', checkAuth);
--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAH25iBbOBoI752_2iz62I0KUiNEPKf22o",
+  authDomain: "nodefirebase-d00d5.firebaseapp.com",
+  databaseURL: "https://nodefirebase-d00d5.firebaseio.com",
+  projectId: "nodefirebase-d00d5",
+  storageBucket: "nodefirebase-d00d5.appspot.com",
+  messagingSenderId: "1018848797651",
+  appId: "1:1018848797651:web:846179ba0748abc2580526"
+});
 
 Vue.config.productionTip = false
 
